6226b167120eaf0d66ea394cbd2ad3119e15c0ff
Yuta Inoue <kfyvic@gmail.cim>
2017-01-07 15:58:59 +0900

    commit

--- app/assets/javascripts/channels/date.js
+++ app/assets/javascripts/channels/date.js
@@ -11,7 +11,8 @@ $(function() {
   });
 
   $('input[name="startDate"]').on('apply.daterangepicker', function(ev, picker) {
-      $(this).val(picker.startDate.format('YYYY/MM/DD') + ' - ' + picker.endDate.format('YYYY/MM/DD'));
+      $(this).val(picker.startDate.format('YYYY/MM/DD'));
+      $('#end-date').val(picker.endDate.format('YYYY/MM/DD'));
   });
 
   $('input[name="startDate"]').on('cancel.daterangepicker', function(ev, picker) {
