bc349b99425465673e348cb08889938ff0f6bdac
Yuta Inoue <kfyvic@gmail.cim>
2017-01-09 17:38:58 +0900

    commit

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,50 +1,50 @@
-$(function(){
-  $(window).on("load", function() {
-    var page = 1;
-    var flag = false;
-    $(window).scroll(function() {
-      console.log("スクロール中")
-      var scrollHeight = $(document).height();
-      var scrollPosition = $(window).height() + $(window).scrollTop();
-      if ((scrollHeight - scrollPosition) <= 2 && page < 8 && !flag) {
-        console.log("今よ！！");
-        setTimeout(function(){
-          page++;
-          $.ajax({
-            type: 'GET',
-            url: '/rooms',
-            dataType: 'json',
-            data: {page: page}
-          }).done(function(data){
+// $(function(){
+//   $(window).on("load", function() {
+//     var page = 1;
+//     var flag = false;
+//     $(window).scroll(function() {
+//       console.log("スクロール中")
+//       var scrollHeight = $(document).height();
+//       var scrollPosition = $(window).height() + $(window).scrollTop();
+//       if ((scrollHeight - scrollPosition) <= 2 && page < 8 && !flag) {
+//         console.log("今よ！！");
+//         setTimeout(function(){
+//           page++;
+//           $.ajax({
+//             type: 'GET',
+//             url: '/rooms',
+//             dataType: 'json',
+//             data: {page: page}
+//           }).done(function(data){
 
-            $('#index-row').append(
-              '<div class="col-md-4 margin-48px">' +
-              '<img src=' + data.room_0_image_url + '>' +
-              '<p class="margin-12px">Grand and Cozy 1920\'s SF Studio</p>' +
-              '</div>'
-            );
+//             $('#index-row').append(
+//               '<div class="col-md-4 margin-48px">' +
+//               '<img src=' + data.room_0_image_url + '>' +
+//               '<p class="margin-12px">Grand and Cozy 1920\'s SF Studio</p>' +
+//               '</div>'
+//             );
 
-            $('#index-row').append(
-              '<div class="col-md-4 margin-48px">' +
-              '<img src=' + data.room_1_image_url + '>' +
-              '<p class="margin-12px">Grand and Cozy 1920\'s SF Studio</p>' +
-              '</div>'
-            );
+//             $('#index-row').append(
+//               '<div class="col-md-4 margin-48px">' +
+//               '<img src=' + data.room_1_image_url + '>' +
+//               '<p class="margin-12px">Grand and Cozy 1920\'s SF Studio</p>' +
+//               '</div>'
+//             );
 
-            $('#index-row').append(
-              '<div class="col-md-4 margin-48px">' +
-              '<img src=' + data.room_2_image_url + '>' +
-              '<p class="margin-12px">Grand and Cozy 1920\'s SF Studio</p>' +
-              '</div>'
-            );
+//             $('#index-row').append(
+//               '<div class="col-md-4 margin-48px">' +
+//               '<img src=' + data.room_2_image_url + '>' +
+//               '<p class="margin-12px">Grand and Cozy 1920\'s SF Studio</p>' +
+//               '</div>'
+//             );
 
-            flag = false;
-          }).fail(function(xhr, status, error){
-            console.log("エラー");
-          });
-        }, 1000);
-      }
-    });
-  });
-});
+//             flag = false;
+//           }).fail(function(xhr, status, error){
+//             console.log("エラー");
+//           });
+//         }, 1000);
+//       }
+//     });
+//   });
+// });
 
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,16 +1,16 @@
 class RoomsController < ApplicationController
   def index
-    @rooms = Room.page(params[:page]).per(3)
-    # @exes = Experience.all
-    respond_to do |format|
-      format.html
-      format.json { render json: {
-                                    room_0_image_url: @rooms[0].images.first.image_url,
-                                    room_1_image_url: @rooms[1].images.first.image_url,
-                                    room_2_image_url: @rooms[2].images.first.image_url
-                                  }
-                  }
-    end
+    @rooms = Room.page(params[:page])
+
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: {
+    #                                 room_0_image_url: @rooms[0].images.first.image_url,
+    #                                 room_1_image_url: @rooms[1].images.first.image_url,
+    #                                 room_2_image_url: @rooms[2].images.first.image_url
+    #                               }
+    #               }
+    # end
   end
   def show
     @room = Room.find(params[:id])
