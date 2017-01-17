e42f926aeb16c78bbabb61dce489926ea1ee558a
Yuta Inoue <kfyvic@gmail.cim>
2017-01-17 21:13:28 +0900

    install-geocoder

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -19,16 +19,40 @@ class RoomsController < ApplicationController
   end
 
   def new
-    @room = Room.new
+    @room = current_user.rooms.build
   end
 
   def create
-    @room = Room.create(room_params)
-    redirect_to root_path
+    @room = current_user.rooms.build(room_params)
+
+    if @room.save
+      redirect_to root_path, notice: "リスティングを作成・保存をしました"
+    else
+      redirect_to new_room_path, notice: "リスティングを作成・保存出来ませんでした"
+    end
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to root_path, notice: "リスティングを編集しました"
+    else
+      redirect_to new_room_path, notice: "リスティングを作成・保存出来ませんでした"
+    end
+  end
+
+
+  def search
+    @latitude = params[:lat]
+    @longitude = params[:lng]
   end
 
   private
   def room_params
-    params.require(:room).permit(:name, :detail, :image, :price, :room_type, :capacity, :bedroom_num, :bed_num, :city)
+    params.require(:room).permit(:name, :detail, :image, :price, :room_type, :capacity, :bedroom_num, :bed_num, :address)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,9 @@
 class Room < ApplicationRecord
+  belongs_to :user
   has_many :images, as: :imageable
   has_many :reservations
   accepts_nested_attributes_for :images
+
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 end
